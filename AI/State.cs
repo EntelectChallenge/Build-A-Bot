d9676fc26cc987e583400b853b521cf9c00c94ca
Natasha Draper <natasha@draper.net.za>
2023-06-25 19:17:25 +0200

    Add state machine structure

---
+++ AI/State.cs
@@ -0,0 +1,22 @@
+﻿using BuildABot.Enums;
+using BuildABot.Models;
+
+namespace BuildABot.AI
+{
+    public abstract class State
+    {
+        protected StateMachine SM;
+
+        protected State(StateMachine SM)
+        {
+            this.SM = SM;
+        }
+
+        protected void ChangeState(State NewState)
+        {
+            SM.ChangeState(NewState);
+        }
+
+        public abstract InputCommand ProcessState(BotStateDTO BotState);
+    }
+}
---
+++ AI/StateMachine.cs
@@ -0,0 +1,20 @@
+﻿using BuildABot.Enums;
+using BuildABot.Models;
+
+namespace BuildABot.AI
+{
+    public class StateMachine
+    {
+        private State CurrentState;
+
+        public void ChangeState(State NewState)
+        {
+            CurrentState = NewState;
+        }
+
+        public InputCommand ProcessState(BotStateDTO BotState)
+        {
+            return CurrentState.ProcessState(BotState);
+        }
+    }
+}
