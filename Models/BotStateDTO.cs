a467c939e9e109871cac83b90446cac3bb277b08
Natasha Draper <natasha@draper.net.za>
2023-06-25 14:15:12 +0200

    Add ToString to bot state

--- Models/BotStateDTO.cs
+++ Models/BotStateDTO.cs
@@ -12,5 +12,30 @@
         public int X { get; set; }
         public int Y { get; set; }
         public int[][] RadarData { get; set; }
+
+        public override string ToString()
+        {
+            // Early return if we don't have a window.
+            if (HeroWindow == null || HeroWindow[0] == null)
+            {
+                return "";
+            }
+            String result = "";
+
+            // Print metadata.
+            result += $"Level: {CurrentLevel}\tState: {CurrentState}\tConnection ID: {ConnectionId}\n";
+            result += $"Collected: {Collected}\tElapsed Time: {ElapsedTime}\tGame Tick: {GameTick}\n";
+            result += $"Position: ({X}, {Y})\n\n";
+            // Print hero window.
+            for (int y = 0; y < HeroWindow[0].Length; y++)
+            {
+                for (int x = 0; x < HeroWindow.Length; x++)
+                {
+                    result += HeroWindow[x][y] + " ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
     }
 }
