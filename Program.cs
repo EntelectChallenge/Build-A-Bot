f5aa0be63f18107005b7152e29ee536c94332bbb
Natasha Draper <natasha@draper.net.za>
2023-06-25 10:52:29 +0200

    Read config & initialise SignalR connection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BuildABot
+{
+    public class Program
+    {
+        private static IConfigurationRoot Configuration;
+
+        private static void Main(string[] args)
+        {
+            // Setup configuration builder with appsettings.json
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false);
+
+            // Read appsettings.json into Configuration
+            Configuration = builder.Build();
+
+            // Retrieve RUNNER_IPV4 environment variable.
+            var environmentIp = Environment.GetEnvironmentVariable("RUNNER_IPV4");
+            // Use RUNNER_IPV4 environment variable if it exists,
+            // else read the value in appsettings.json
+            var runnerIp = !string.IsNullOrWhiteSpace(environmentIp)
+                ? environmentIp
+                : Configuration.GetSection("RunnerIP").Value!;
+            // Add http:// to start if not already present.
+            if (!runnerIp.StartsWith("http://"))
+            {
+                runnerIp = $"http://{runnerIp}";
+            }
+
+            // Retrieve bot nickname from either environment variable or appsettings.json
+            var botNickname = Environment.GetEnvironmentVariable("BOT_NICKNAME")
+                ?? Configuration.GetSection("BotNickname").Value;
+
+            // Get registration token from environment.
+            var token = Environment.GetEnvironmentVariable("Token");
+            // Get runner hub port
+            var port = Configuration.GetSection("RunnerPort").Value;
+            // Build runner hub connection URL.
+            var runnerHubUrl = $"{runnerIp}:{port}/runnerhub";
+
+            // Build SignalR connection
+            var connection = new HubConnectionBuilder()
+                .WithUrl(runnerHubUrl)
+                .ConfigureLogging(logging =>
+                {
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                })
+                .WithAutomaticReconnect()
+                .Build();
+
+            // Start connection
+            connection.StartAsync().Wait();
+            Console.WriteLine("Connected to Runner!");
+        }
+    }
+}
