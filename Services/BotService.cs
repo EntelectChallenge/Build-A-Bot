bb406565553a887c9490719ad0d51e7742a6d122
Natasha Draper <natasha@draper.net.za>
2023-06-25 15:01:55 +0200

    Add bot service

---
+++ Services/BotService.cs
@@ -0,0 +1,25 @@
+﻿using BuildABot.Enums;
+using BuildABot.Models;
+
+namespace BuildABot.Services
+{
+    public class BotService
+    {
+        private Guid BotId;
+
+        public BotCommand ProcessState(BotStateDTO botState)
+        {
+            // Simply return right for now.
+            return new BotCommand
+            {
+                BotId = BotId,
+                Action = InputCommand.RIGHT,
+            };
+        }
+
+        public void SetBotId(Guid botId)
+        {
+            BotId = botId;
+        }
+    }
+}
