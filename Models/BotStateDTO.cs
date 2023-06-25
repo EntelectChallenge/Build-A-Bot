62cc79c03ca527dfdf4d0819d3e783bb58583fac
Natasha Draper <natasha@draper.net.za>
2023-06-25 14:14:49 +0200

    Add bot state model

---
+++ Models/BotStateDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace BuildABot.Models
+{
+    public class BotStateDTO
+    {
+        public int CurrentLevel { get; set; }
+        public string CurrentState { get; set; }
+        public string ConnectionId {  get; set; }
+        public int Collected { get; set; }
+        public string ElapsedTime { get; set; }
+        public int GameTick { get; set; }
+        public int[][] HeroWindow { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int[][] RadarData { get; set; }
+    }
+}
