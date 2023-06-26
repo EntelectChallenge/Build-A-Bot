2f918a9c271e50397443a89e945f413fb4ec2e46
Natasha Draper <natasha@draper.net.za>
2023-06-26 14:21:44 +0200

    Add point datastructure

---
+++ AI/DataStructures/Spatial/Point.cs
@@ -0,0 +1,34 @@
+﻿namespace BuildABot.AI.DataStructures.Spatial
+{
+    public class Point : IEquatable<Point>
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        bool IEquatable<Point>.Equals(Point? other)
+        {
+            return other is not null && X == other.X && Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public static Point operator+(Point a, Point b)
+        {
+            return new(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Point operator-(Point a, Point b)
+        {
+            return new(a.X - b.X, a.Y - b.Y);
+        }
+    }
+}
