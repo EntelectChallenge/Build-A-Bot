8ce6f29bdf6236877260d8432941d580ec5bf0b9
Natasha Draper <natasha@draper.net.za>
2024-07-07 19:57:24 +0200

    2024: Add configuration and connecting to hub

--- Build-A-Bot/Program.cs
+++ Build-A-Bot/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+IConfigurationRoot configuration;
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false);
+configuration = builder.Build();
+
+var runnerIp = Environment.GetEnvironmentVariable("RUNNER_IPV4")
+ ?? configuration.GetSection("RunnerIP").Value!;
+
+if (!runnerIp.StartsWith("http://"))
+{
+    runnerIp = $"http://{runnerIp}";
+}
+
+var botNickname = Environment.GetEnvironmentVariable("BOT_NICKNAME")
+    ?? configuration.GetSection("BotNickname").Value;
+
+var token = Environment.GetEnvironmentVariable("Token");
+var port = configuration.GetSection("RunnerPort").Value;
+var runnerHubUrl = $"{runnerIp}:{port}/runnerhub";
+
+var connection = new HubConnectionBuilder()
+    .WithUrl(runnerHubUrl)
+    .ConfigureLogging(logging =>
+    {
+        logging.SetMinimumLevel(LogLevel.Debug);
+    })
+    .WithAutomaticReconnect()
+    .Build();
+
+connection.StartAsync().Wait();
+Console.WriteLine("Connected to Runner!");
