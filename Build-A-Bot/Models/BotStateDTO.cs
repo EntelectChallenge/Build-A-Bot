1b40da36ed8f6c1f095e5ec8cc9b3ab13ecf290c
Natasha Draper <natasha@draper.net.za>
2024-07-07 20:04:47 +0200

    2024: Add BotStateDTO

---
+++ Build-A-Bot/Models/BotStateDTO.cs
@@ -0,0 +1,27 @@
+namespace BuildABot.Models;
+public class BotStateDTO
+{
+    public int DirectionState { get; set; }
+    public required string ElapsedTime { get; set; }
+    public int GameTick { get; set; }
+    public int PowerUp { get; set; }
+    public int SuperPowerUp { get; set; }
+    public required Dictionary<Guid, int> LeaderBoard { get; set; }
+    public required Location[] BotPostions { get; set; }
+    public required PowerUpLocation[] PowerUpLocations { get; set; }
+    public required bool[][] Weeds { get; set; }
+    public required int[][] HeroWindow { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+public struct PowerUpLocation
+{
+    public Location Location { get; set; }
+    public int Type { get; set; }
+}
+
+public struct Location
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
